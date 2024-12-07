be93195a40b2ee018f90e66f1307e13a34470123
Rajkumar Rathod <rajkumarrathod414@gmail.com>
2024-12-07 14:53:32 +0530

    changes for deploy page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import  { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 // import './index.css'; // Your global styles (optional)
